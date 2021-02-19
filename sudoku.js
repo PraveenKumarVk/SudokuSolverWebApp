1e270fbc4fe555c60d2d4151d54a9e30b644387a
PraveenKumarVk <64854489+PraveenKumarVk@users.noreply.github.com>
2021-02-19 14:54:22 +0530

    Update sudoku.js

--- sudoku.js
+++ sudoku.js
@@ -1,11 +1,25 @@
-var arr=[[0,0,0,0,0,0,0,0,0],[0,0,0,0,0,0,0,0,0],[0,0,0,0,0,0,0,0,0],[0,0,0,0,0,0,0,0,0],[0,0,0,0,0,0,0,0,0],[0,0,0,0,0,0,0,0,0],[0,0,0,0,0,0,0,0,0],[0,0,0,0,0,0,0,0,0],[0,0,0,0,0,0,0,0,0]];
-var board=[[0,0,0,0,0,0,0,0,0],[0,0,0,0,0,0,0,0,0],[0,0,0,0,0,0,0,0,0],[0,0,0,0,0,0,0,0,0],[0,0,0,0,0,0,0,0,0],[0,0,0,0,0,0,0,0,0],[0,0,0,0,0,0,0,0,0],[0,0,0,0,0,0,0,0,0],[0,0,0,0,0,0,0,0,0]];
+var arr=[];
+var board=[];
 var beginning;
-function solver(){
+var sz;
+var sqSize;
+function solver(size){
+    sz=size;
+    var unit1=[],unit2=[];
+    for(var i=0;i<sz;i++){
+        for(var j=0;j<sz;j++){
+            unit1.push(0);
+            unit2.push(0);
+        }
+        arr.push(unit1);
+        board.push(unit2);
+        unit2=[],unit1=[];
+    }
+    sqSize=Math.floor(Math.sqrt(size));
     document.getElementById("wrong").style.visibility="hidden";
-    for (var i=0;i<9;i++){
-        for (var j=0;j<9;j++){
-            var x=document.getElementById(i.toString()+j.toString()).value.toString();
+    for (var i=0;i<size;i++){
+        for (var j=0;j<size;j++){
+            var x=document.getElementById("r"+i.toString()+"c"+j.toString()).value.toString();
             if(isNaN(x)){
                 if(x=="")
                 arr[i][j]=0;
@@ -17,8 +31,8 @@ function solver(){
     }
     beginning=JSON.parse(JSON.stringify(arr));
     var valid=true;
-    for(var i=0;i<9;i++){
-        for(var j=0;j<9;j++){
+    for(var i=0;i<size;i++){
+        for(var j=0;j<size;j++){
             if(arr[i][j]!=0){
                 valid=dup_validity(i,j,arr[i][j]);
             }
@@ -35,6 +49,7 @@ function solver(){
     }
     else{
         document.getElementById("wrong").style.visibility="visible";
+        reset(sz);
     }
 }
 function sleep(ms){
@@ -43,23 +58,23 @@ function sleep(ms){
 
     }while(present-Date.now()>0);
 }
-function fillBox(arra){
-    for(var i=0;i<9;i++){
-        for(var j=0;j<9;j++){
+function fillBox(arr){
+    for(var i=0;i<sz;i++){
+        for(var j=0;j<sz;j++){
             if(arr[i][j]!=beginning[i][j]){
-                document.getElementById(i.toString()+j.toString()).style.backgroundColor="#111d5e";
-                document.getElementById(i.toString()+j.toString()).style.color="#ffffff";
+                document.getElementById("r"+i.toString()+"c"+j.toString()).style.backgroundColor="#111d5e";
+                document.getElementById("r"+i.toString()+"c"+j.toString()).style.color="#ffffff";
             }
             else
-            document.getElementById(i.toString()+j.toString()).style.backgroundColor="#008891";
-            document.getElementById(i.toString()+j.toString()).value=arr[i][j];
+            document.getElementById("r"+i.toString()+"c"+j.toString()).style.backgroundColor="#008891";
+            document.getElementById("r"+i.toString()+"c"+j.toString()).value=arr[i][j];
         }
     }
 }
 
 function dup_isInRow(row,no){
     var c=0;
-    for(var i=0;i<9;i++)
+    for(var i=0;i<sz;i++)
         if(arr[row][i]==no)
         c++;
     if(c>1)
@@ -69,7 +84,7 @@ function dup_isInRow(row,no){
 // private boolean
 function dup_isInCol(col,no){
     var c=0;
-    for(var i=0;i<9;i++)
+    for(var i=0;i<sz;i++)
         if(arr[i][col]==no)
         c++;
     if(c>1)
@@ -78,11 +93,11 @@ function dup_isInCol(col,no){
 }
 // private boolean 
 function dup_isInBox(row,col,no){
-    var r=row-row%3;
-    var c=col-col%3;
+    var r=row-row%sqSize;
+    var c=col-col%sqSize;
     var cnt=0;
-    for(var i=r;i<r+3;i++)
-        for(var j=c;j<c+3;j++)
+    for(var i=r;i<r+sqSize;i++)
+        for(var j=c;j<c+sqSize;j++)
             if(arr[i][j]==no)
             cnt++;
     if(cnt>1)
@@ -96,7 +111,7 @@ function dup_validity(row,col,no){
 }
     function isInRow(row,no){
         var c=0;
-        for(var i=0;i<9;i++)
+        for(var i=0;i<sz;i++)
             if(arr[row][i]==no)
             return true;
         return false;
@@ -104,18 +119,18 @@ function dup_validity(row,col,no){
     // private boolean
     function isInCol(col,no){
         var c=0;
-        for(var i=0;i<9;i++)
+        for(var i=0;i<sz;i++)
             if(arr[i][col]==no)
             return true;
         return false;
     }
     // private boolean 
     function isInBox(row,col,no){
-        var r=row-row%3;
-        var c=col-col%3;
+        var r=row-row%sqSize;
+        var c=col-col%sqSize;
         var cnt=0;
-        for(var i=r;i<r+3;i++)
-            for(var j=c;j<c+3;j++)
+        for(var i=r;i<r+sqSize;i++)
+            for(var j=c;j<c+sqSize;j++)
                 if(arr[i][j]==no)
                 return true;
         return false;
@@ -127,10 +142,10 @@ function dup_validity(row,col,no){
     }
     
     function solve(){
-        for(var row=0;row<9;row++){
-            for(var col=0;col<9;col++){
+        for(var row=0;row<sz;row++){
+            for(var col=0;col<sz;col++){
                 if(arr[row][col]==0){
-                    for(var no=1;no<=9;no++){
+                    for(var no=1;no<=sz;no++){
                         if (validity(row,col,no)){
                             arr[row][col]=no;
 
@@ -145,11 +160,15 @@ function dup_validity(row,col,no){
         }
         return true;
     }
-function reset(){
-    for(var i=0;i<9;i++){
-        for(var j=0;j<9;j++){
-            document.getElementById(i.toString()+j.toString()).value="";
-            document.getElementById(i.toString()+j.toString()).style.backgroundColor="white";
+function reset(len){
+    arr=[];
+    board=[];
+    for(var i=0;i<len;i++){
+        for(var j=0;j<len;j++){
+            document.getElementById("r"+i.toString()+"c"+j.toString()).style.backgroundColor="";
+            document.getElementById("r"+i.toString()+"c"+j.toString()).style.caretColor="black";
+            document.getElementById("r"+i.toString()+"c"+j.toString()).style.color="black";
+            document.getElementById("r"+i.toString()+"c"+j.toString()).value="";
         }
     }
 }
